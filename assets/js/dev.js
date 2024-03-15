25cdbbc9721886a3489d2ccd41441cfdc2c5c282
jhonatanq <jhonatan_q.r90@hotmail.com>
2024-03-15 13:25:54 -0500

    Inicio de proyecto

---
+++ assets/js/dev.js
@@ -0,0 +1,35 @@
+'use strict';
+const site = (function () {
+
+    const DOM = {
+        ACTIVE_CLASS: '-active-',
+        SHOW_CLASS: '-show-',
+        HIDING_CLASS: '-hiding-',
+        COMPLETED_CLASS: '-completed-',
+        DISABLED_CLASS: '-disabled-'
+    };
+
+    const siteFunctions = function () {
+
+        // events.header();
+
+    };
+
+    const events = {
+
+        header: function () {
+
+
+
+        },
+
+    }
+
+
+    let initialize = siteFunctions;
+
+    return { init: initialize }
+
+})();
+
+site.init();
---
+++ assets/js/main.js
@@ -0,0 +1,1 @@
+"use strict";const site={init:function(){}};site.init();

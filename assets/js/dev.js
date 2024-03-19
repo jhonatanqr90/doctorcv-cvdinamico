5597ac9027b20df5afaa407a48da069be26be58f
jhonatanq <jhonatan_q.r90@hotmail.com>
2024-03-19 00:49:26 -0500

    avance 4

--- assets/js/dev.js
+++ assets/js/dev.js
@@ -12,6 +12,7 @@ const site = (function () {
     const siteFunctions = function () {
 
         events.header();
+        events.cv();
 
     };
 
@@ -22,6 +23,61 @@ const site = (function () {
             const $cv = document.getElementById('cv')
             console.log($cv.offsetHeight)
         },
+        cv: function () {
+
+            const $_fnShowFileDetail = document.querySelectorAll('.fnShowFileDetail')
+            const $_closeFileDetail = document.querySelectorAll('.closeFileDetail')
+
+            const $_fnShowVideoDetail = document.querySelectorAll('.fnShowVideoDetail')
+            const $_fnCloseVideoDetail = document.querySelectorAll('.fnCloseVideoDetail')
+
+
+            const $fileDetail = document.getElementById('fileDetail')
+            const $videoDetail = document.getElementById('videoDetail')
+            const $videoDetailIframe = document.getElementById('videoDetailIframe')
+
+            // Detalle de texto
+            $_fnShowFileDetail.forEach($button => {
+                $button.addEventListener('click', () => {
+                    $fileDetail.classList.add(DOM.ACTIVE_CLASS)
+                    setTimeout(() => {
+                        $fileDetail.classList.add(DOM.SHOW_CLASS)
+                    }, 10)
+                })
+            })
+
+            $_closeFileDetail.forEach($button => {
+                $button.addEventListener('click', () => {
+                    $fileDetail.classList.remove(DOM.SHOW_CLASS)
+                    setTimeout(() => {
+                        $fileDetail.classList.remove(DOM.ACTIVE_CLASS)
+                    }, 300)
+                })
+            })
+
+            // Detalle de video
+            $_fnShowVideoDetail.forEach($button => {
+                $button.addEventListener('click', () => {
+                    $videoDetail.classList.add(DOM.ACTIVE_CLASS)
+                    setTimeout(() => {
+                        $videoDetailIframe.src = 'https://www.youtube.com/embed/pRxfifDkmCk?si=fiDCD0wWZn7wVuNn'
+                        $videoDetail.classList.add(DOM.SHOW_CLASS)
+                    }, 10)
+                })
+            })
+
+            $_fnCloseVideoDetail.forEach($button => {
+                $button.addEventListener('click', () => {
+                    $videoDetail.classList.remove(DOM.SHOW_CLASS)
+                    setTimeout(() => {
+                        $videoDetail.classList.remove(DOM.ACTIVE_CLASS)
+                        $videoDetailIframe.src = ''
+                    }, 300)
+                })
+            })
+
+
+        }
 
     }
 
